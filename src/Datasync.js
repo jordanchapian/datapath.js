bea514d81a4675ba7205a653fc1114100d5793e6
Jordan D Chapian <jchapian@pyaanalytics.com>
2015-12-15 22:46:09 -0500

    added base module structure. added typechecking namespace. added typechecking utilities.

---
+++ src/Datasync.js
@@ -0,0 +1,80 @@
+var Datasync = {
+	_:{
+		factories:{
+			pipeline:{}
+		},
+		state:{
+
+		},
+		util:{
+			is:{}
+		}
+	}
+};
+
+/*================================
+=            Pipeline            =
+================================*/
+/*----------  Formatter  ----------*/
+
+(function(){
+	
+	function Formatter(){
+
+	}
+
+
+	Datasync.factories.Formatter = function(){
+		return (new formatter());
+	};
+
+	Datasync.addSubset = function(){
+
+	};
+
+})();
+
+
+
+/*=====  End of Pipeline  ======*/
+
+
+/*=============================
+=            Utils            =
+=============================*/
+
+/*----------  Type Checking  ----------*/
+
+Datasync._.util.is.Integer = function(val) {
+	return isNumber(val) && Math.floor(val) == val;
+};
+
+Datasync._.util.is.Number = function(val) {
+	return typeof val == "number";
+};
+
+Datasync._.util.is.String = function(){
+	return typeof val == "string";
+};
+
+Datasync._.util.is.Boolean = function(val) {
+	return typeof val == "boolean";
+};
+
+Datasync._.util.is.Object = function(val) {
+	return typeof val == "object" && val !== null;
+};
+
+Datasync._.util.is.Array = function(val) {
+	return val instanceof Array;
+};
+
+Datasync._.util.is.Function = function(val) {
+	return typeof val == "function";
+};
+
+Datasync._.util.is.Date = function(val) {
+	return (Object.prototype.toString.call(val) === "[object Date]");
+};
+
+/*=====  End of Utils  ======*/
