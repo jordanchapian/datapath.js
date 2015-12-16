6b1b0c86a3ab2f71e9870caa547057fe9a35c246
Jordan D Chapian <jordanchapian@gmail.com>
2015-12-16 17:02:56 -0500

    + added public/private environment functions

--- src/env_intro.js
+++ src/env_intro.js
@@ -1,2 +1,33 @@
 (function(Datasync){
 	'use strict';
+
+	var publicAPI = Datasync,
+			privateAPI = {};
+
+	/*==================================
+	=            Namespaces            =
+	==================================*/
+	function _namespace(str, root) {
+		var chunks = str.split('.');
+		if(!root)
+			root = window;
+		var current = root;
+		for(var i = 0; i < chunks.length; i++) {
+			if (!current.hasOwnProperty(chunks[i]))
+				current[chunks[i]] = {};
+			current = current[chunks[i]];
+		}
+		return current;
+	}
+
+	function public(str){
+		return _namespace(str, publicAPI);
+	}
+
+	function private(str){
+		return _namespace(str, privateAPI);
+	}
+	/*=====  End of Namespaces  ======*/
+
+
+};
