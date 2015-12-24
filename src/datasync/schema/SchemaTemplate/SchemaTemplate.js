75cd3905f1d19a8f5bd481ef342e9525a105b20a
Jordan D Chapian <jordanchapian@gmail.com>
2015-12-24 12:31:45 -0500

    adding a basic schema template class with validation function wireframe.

---
+++ src/datasync/schema/SchemaTemplate/SchemaTemplate.js
@@ -0,0 +1,15 @@
+(function(schemaFactories){
+	function SchemaTemplate(configuration){
+
+	}
+
+	/*----------  class methods  ----------*/
+	
+	/*----------  static methods  ----------*/
+	SchemaTemplate.isValidConfiguration = function(){
+
+	};
+	
+})(
+	_private('schema.factory')
+);
